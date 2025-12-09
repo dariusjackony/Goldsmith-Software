439e4ce793ee200c0f432202ad1303a9a9899106
darius <dariusjackon11@gmail.com>
2025-12-09 17:59:10 +0300

    Update: Added Icons

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,8 @@
 import "./Services.css";
 import Tablet from "../../assets/code.png";
-
-
+import boxes from "../../assets/boxes.png";
+import star from "../../assets/star.png";
+import desktop from "../../assets/tablet.png";
 export default function Services(){
     const ServiceData = [
         {
@@ -10,17 +11,17 @@ export default function Services(){
             description: 'ERP systems, inventory tools, automation workflows, desktop & mobile solution'
         },
         {   
-            image: Tablet,
+            image: boxes,
             title: 'Web & Mobile Apps',
             description: 'Corporate websites, e-commerce platforms, Android/IOS apps, UI/UX design'
         },
         {
-            image: Tablet,
+            image: star,
             title: 'AI & Data Solutions',
             description: 'Chatbots, predictive analytics, machine learning,automation systems'
         },
         {
-            image: Tablet,
+            image: desktop,
             title: 'System Design & Architecture',
             description: 'API development, database design, cloud infrastructure planning'
         }
@@ -32,6 +33,7 @@ export default function Services(){
              <div className="service-content">
                 {ServiceData.map((item,index) =>(
                     <div className="service-card" key={index}>
+                        <img src={item.image}  className="service-img"/>
                         <p className="service-subtitle">{item.title}</p>
                         <p className="service-description">{item.description}</p>
                     </div>

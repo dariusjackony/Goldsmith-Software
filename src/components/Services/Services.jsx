811b8f43f0bde21b4b9eb0651af2dcb6eae2c0d4
darius <dariusjackon11@gmail.com>
2025-12-09 17:28:45 +0300

    Update: import bugs

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,19 +1,26 @@
 import "./Services.css";
+import Tablet from "../../assets/code.png";
+
+
 export default function Services(){
     const ServiceData = [
         {
+            image: Tablet,
             title: 'Custom Software Development',
             description: 'ERP systems, inventory tools, automation workflows, desktop & mobile solution'
         },
-        {
+        {   
+            image: Tablet,
             title: 'Web & Mobile Apps',
             description: 'Corporate websites, e-commerce platforms, Android/IOS apps, UI/UX design'
         },
         {
+            image: Tablet,
             title: 'AI & Data Solutions',
             description: 'Chatbots, predictive analytics, machine learning,automation systems'
         },
         {
+            image: Tablet,
             title: 'System Design & Architecture',
             description: 'API development, database design, cloud infrastructure planning'
         }

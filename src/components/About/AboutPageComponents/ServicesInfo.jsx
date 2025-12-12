075533e0ee9ae4adf4a999d723c40e2c8b193386
darius <dariusjackon11@gmail.com>
2025-12-12 13:36:31 +0300

    Update: services icons

--- src/components/About/AboutPageComponents/ServicesInfo.jsx
+++ src/components/About/AboutPageComponents/ServicesInfo.jsx
@@ -1,41 +1,46 @@
 import "./ServicesInfo.css";
-import srImage from "../../../assets/about-us.gif";
+import aiDev from "../../../assets/ai-dev.jpg";
+import ItSupport from "../../../assets/it-support.jpg";
+import mobileDev from "../../../assets/mobile-and-web-dev.jpg";
+import SoftwareDev from "../../../assets/software-dev.jpg";
+import systemDev from "../../../assets/sys-dev.jpg";
+import wordPress from "../../../assets/wordpress.jpg";
 export default function ServicesInfo(){
     const ServiceData = [
         {
-          Image: srImage,
+          Image: SoftwareDev,
           title: "Custom Software Development",
           description: "Business management systems, ERP & workflow automation Inventory, sales & financial management systems, Desktop & mobile apps",
           tags: ["ERP", "Workflow Automation", "Inventory", "Desktop Apps", "Mobile Apps"]
         },
         {
-          Image: srImage,
+          Image: mobileDev,
           title: "Web & Mobile App Development",
           description: "Corporate websites, E-commerce systems, Android & iOS mobile apps, UI/UX design",
           tags: ["Web Apps", "Mobile Apps", "E-commerce", "UI/UX", "Responsive Design"]
         },
         {
-          Image: srImage,
+          Image: aiDev,
           title: "AI & Data Solutions",
           description: "Chatbots & intelligent assistants, Predictive analytics,Machine learning systems, Computer vision",
           tags: ["AI", "Machine Learning", "Data Analytics", "Chatbots", "Computer Vision"]
 
         },
         {
-          Image: srImage,
+          Image: systemDev,
           title: "System Design & Architecture",
           description: "API development, Database design, Cloud & server infrastructure",
           tags: ["APIs", "Databases", "Cloud", "Microservices", "Scalability"]
 
         },
         {
-          Image: srImage,
+          Image: wordPress,
           title: "Wordpress Design and Maintainance",
           description: "Design business websites fit to grow your business, build and maintain the websites to ensure it's up 24/7",
           tags: ["WordPress", "Elementor", "SEO", "Speed Optimization", "Maintenance"]
         },
         {
-          Image: srImage,
+          Image: ItSupport,
           title: "IT Consultancy & Support",
           description: "Digital transformation consulting, Cybersecurity best practices, System maintenance",
           tags: ["Cloud Support", "IT Support", "System Monitoring"] 

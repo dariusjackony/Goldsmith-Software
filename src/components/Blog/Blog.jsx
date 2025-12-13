d9bf358a342dc382002658e23da62d45b2ef2ade
darius <dariusjackon11@gmail.com>
2025-12-13 18:04:36 +0300

    Update:..

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import Contact from "../../components/Contact/Contact";
+import CodePic from "../../assets/software-dev.jpg";
 import "./Blog.css";
 const blogs = [
-  { id: "web-development-tips", title: "Top 5 Web Development Tips", content:"Learn the essential tips for building fast, responsive, and modern websites that delight users." },
-  { id: "ai-in-software", title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
-  { id: "ai-in-software", title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
-  { id: "ai-in-software", title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
-  { id: "ai-in-software", title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
-  { id: "ai-in-software", title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
+  { id: "web-development-tips", image: CodePic, title: "Top 5 Web Development Tips", content:"Learn the essential tips for building fast, responsive, and modern websites that delight users." },
+  { id: "ai-in-software", image: CodePic, title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
+  { id: "ai-in-software", image: CodePic, title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
+  { id: "ai-in-software", image: CodePic, title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
+  { id: "ai-in-software", image: CodePic,  title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
+  { id: "ai-in-software", image: CodePic,  title: "How AI is Changing Software Development", content:"Explore how artificial intelligence is revolutionizing software development, from automation to smarter apps." },
    
 ];
 
@@ -16,16 +17,19 @@ function Blogs() {
     <div className="blogs-main-container">
       <span className="blog-head">Our Blogs</span>
       <p className="blog-intro">
-      Discover the latest insights, tips, and guides from Goldsmith Softwares Limited your source for web development, software solutions, and professional consultations.
+        Discover the latest insights, tips, and guides from Goldsmith Softwares Limited your source for web development, software solutions, and professional consultations.
       </p>
       <div className="blogs-content">
         {blogs.map((blog) => (
         <div key={blog.id} className="blog-card">
-          <p className="blog-title">{blog.title}</p>
+          <img src={CodePic} className="blog-img" />
+          <div className="card-details">
+            <p className="blog-title">{blog.title}</p>
           <p className="blog-subtitle">{blog.content}</p>
           <Link to={`/blogs/${blog.id}`}>
              <button className="read-btn">Read More</button>
           </Link>
+          </div>
         </div>
         ))}
       </div>

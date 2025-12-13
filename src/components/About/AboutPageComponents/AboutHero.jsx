a8183aeab7d47397b2f1988d63dadd417c732185
darius <dariusjackon11@gmail.com>
2025-12-13 07:36:47 +0300

    Update:...

--- src/components/About/AboutPageComponents/AboutHero.jsx
+++ src/components/About/AboutPageComponents/AboutHero.jsx
@@ -5,11 +5,9 @@ export default function AboutHero(){
             <div className="about-hero-content">
                 <span className="abt-head">About Goldsmith Softwares</span>
                 <p className="abt-head-paragraph">
-                     Goldsmith Softwares is a modern technology company dedicated to building custom software
-                     and intelligent digital systems for businesses. We craft every digital solution with precision just 
-                     like a goldsmith crafts fine jewelry.
-                     We deliver reliable, scalable, and secure systems that help organizations automate processes,
-                     improve workflows, and embrace digital transformation.
+                      Goldsmith Softwares is a modern technology company building custom software and intelligent digital
+                      systems for businesses. We craft precise, secure, and scalable solutions that help organizations
+                      automate processes, improve workflows, and embrace digital transformation.
                 </p>
             </div>
         </section>
